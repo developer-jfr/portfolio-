b3a68bdff1341346e4105d3483902eae7e14323b
frdevelopergen <erkinbaevbakai8@gmail.com>
2022-08-22 22:31:54 +0600

    first commit

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BsInstagram } from 'react-icons/bs';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://www.instagram.com/erkinbaevbakai/"
+    >
+      <div>
+        <BsInstagram />
+      </div>
+    </a>
+  </div>
+);
+
+export default SocialMedia;
