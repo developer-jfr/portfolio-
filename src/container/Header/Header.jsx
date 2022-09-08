5a8560975b54852ea6aa5aa680c88986f4e6ef61
frdevelopergen <erkinbaevbakai8@gmail.com>
2022-09-08 17:21:59 +0600

    chages

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => (
           <span>👋</span>
           <div style={{ marginLeft: 20 }}>
             <p className="p-text">Hello, I am</p>
-            <h1 className="head-text">Bakai</h1>
+            <h1 className="head-text">Janara</h1>
           </div>
         </div>
 
